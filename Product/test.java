beb80b2bce798ea797f29796cf50a4944d4ad512
dalicanka <daniyalosama@outlook.com>
2025-06-08 01:03:23 -0700

    added hello 2

--- Product/test.java
+++ Product/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println ("This is a test for the Product package.");
+        System.out.println("Hello 2");
     }
 }
