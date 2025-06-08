f7c4698a4ce64606b8488be5ef74be3aabe2c3a5
dalicanka <daniyalosama@outlook.com>
2025-06-08 01:01:10 -0700

    code update

--- Product/test.java
+++ Product/test.java
@@ -3,5 +3,6 @@ package Product;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println ("This is a test for the Product package.");
     }
 }
